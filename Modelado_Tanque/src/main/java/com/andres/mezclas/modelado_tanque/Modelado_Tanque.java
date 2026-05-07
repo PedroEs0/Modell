aea8c56894313c35d2ca7141061608dc6284d539
ivan <andresivan0807@gmail.com>
2026-05-07 06:41:24 -0500

    subiendo el codigo

---
+++ Modelado_Tanque/src/main/java/com/andres/mezclas/modelado_tanque/Modelado_Tanque.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.andres.mezclas.modelado_tanque;
+import Interfaz.Tanque;
+/**
+ *
+ * @author andre
+ */
+public class Modelado_Tanque {
+
+    public static void main(String[] args) {
+        Tanque pantalla = new Tanque();
+        pantalla.setVisible(true);
+    }
+}
